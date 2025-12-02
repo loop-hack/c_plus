02b035c5673ea83e897c7ae6fb49152bb7dd5f57
loop-hack <timetraveller1732@gmail.com>
2025-12-02 20:58:17 +0530

    comment in c++

--- 1st_code.cpp
+++ 1st_code.cpp
@@ -3,8 +3,9 @@
 int main(){
 	std::cout << "Hello, World!" << std::endl;
 	std::cout << "Hey, Ankit Anurag" << std::endl;
-	std::cout << "Number1";
-	std::cout << "Number2";
+	std::cout << "Number1"; /*this is how we put comments in c++.
+				  This is a multiline comments */ 
+	std::cout << "Number2"; // This is one line comment.
 	return 0;
 }
 
