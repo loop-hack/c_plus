eb49bbac3ba2e64fb7ae079741a79c4f879ff3d7
loop-hack <timetraveller1732@gmail.com>
2025-12-02 22:34:31 +0530

    warning

---
+++ warning.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main(){
+	std::cout << "Hello, World!" << std::endl;
+
+	//Runtime Error
+	
+	//Runtime Error
+	
+
+	int value = 7/0;
+
+	std::cout << "Value : " << value << std::endl;
+	
+	return 0;
+}
