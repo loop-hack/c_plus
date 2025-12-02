19fe759a6f58dc29d86fc1314e60928f9244ef1b
loop-hack <timetraveller1732@gmail.com>
2025-12-02 22:41:00 +0530

    sum of numbers in c++

---
+++ statment.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main(){
+	int firstnumber = 5;
+	int secondnumber = 10;
+	int sum = firstnumber + secondnumber;
+	std::cout << "The sum of numbers are " << sum << std::endl;
+	return 0;
+}
