782e44c50fb69564dccfd84ead24a55b5c7ed332
loop-hack <timetraveller1732@gmail.com>
2025-12-09 15:05:57 +0530

    error printing

---
+++ error.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main(){
+
+	// Use of std::cout
+	
+	std::cout << "The age is : " << 12 << std::endl;
+
+	// Use of std::cerr for display of error
+	
+	std::cerr << "std::cerr output : Something is wrong" <<std::endl;
+
+	// Use of std::clog to print log messages
+	
+	std::clog << "std::clog output : This is a log message" << std::endl;
+
+	return 0;
+
+
+}
