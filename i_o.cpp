c0645a1eeb4270d56c5ed631f753fae57c03c373
loop-hack <timetraveller1732@gmail.com>
2025-12-09 15:49:55 +0530

    input form users

--- i_o.cpp
+++ i_o.cpp
@@ -3,7 +3,7 @@
 
 int main(){
 
-	int age;
+/*	int age;
 	std::string name;
 
 	std::cout << "Please give your name : " << std::endl;
@@ -17,12 +17,27 @@ int main(){
 	std::cout << "Hello " << name << "! You are " << age << " years old." << std::endl;
 
 
-	/* We can also take both input at the same time */
+	// We can also take both input at the same time 
 
 	std::cout << "Give your name and age : " << std::endl;
 	std::cin >>name >> age;
 
 	std::cout << "Hi " << name << " You are " << age << " years old." << std::endl;
+
+*/
+
+	// To take full name 
+
+	std::string full_name;
+	int age1;
+
+	std::cout << "give full name and age : " << std::endl;
+
+	std::getline(std::cin, full_name);
+
+	std::cin >> age1;
+
+	std::cout << "Namaskar " << full_name << " You are " << age1 << " years old." << std::endl;
 
 	return 0;
 
