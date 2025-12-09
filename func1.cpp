d1ee53c8e49c98a4c5b65ffcee933a9e382f9c01
loop-hack <timetraveller1732@gmail.com>
2025-12-09 14:29:24 +0530

    created a file

---
+++ func1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+
+int main(){
+
+	int firstnum {14};
+	int secondnum {16};
+
+	std::cout << "First Number: "<< firstnum << std::endl;
+	std::cout << "Second Numebr: " << secondnum << std::endl;
+
+	return 0;
+}
